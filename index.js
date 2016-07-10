f5dd2a4835fdac7c73a320f9630c93dcd8702db7
Jackkell <bronzeeagle1@hotmail.com>
2016-07-10 02:10:18 -0800

    You can increment and decrement things

--- index.js
+++ index.js
@@ -1,29 +1,39 @@
-const remote = require('electron').remote
-const main = remote.require('./main.js')
+const remote = require('electron').remote;
+const main = remote.require('./main.js');
+const jsonfile = require('jsonfile');
 
-let count = document.getElementById('count')
+const dataFile = __dirname + '/data.json';
+
+let count = document.getElementById('count');
+
+jsonfile.readFile(dataFile, function(err, obj) {
+	count.textContent = obj.count;
+});
 
 function addCount(delta) {
-	count.textContent = Number(count.textContent) + delta
+	count.textContent = Number(count.textContent) + delta;
+	let object = {count: count.textContent};
+	jsonfile.writeFile(dataFile, object, function(err) {
+		console.error(err);
+	});
 }
 
-
-let incButton = document.createElement('button')
-incButton.textContent = 'Increment'
+let incButton = document.createElement('button');
+incButton.textContent = 'Increment';
 
 incButton.addEventListener('click', () => {
-	addCount(1)
-	console.log(count)
-}, false)
+	addCount(1);
+	console.log(count);
+}, false);
 
-let decButton = document.createElement('button')
-decButton.textContent = 'Decrement'
+let decButton = document.createElement('button');
+decButton.textContent = 'Decrement';
 
 decButton.addEventListener('click', () => {
-	addCount(-1)
-	console.log(count)
-}, false)
+	addCount(-1);
+	console.log(count);
+}, false);
 
 
-document.body.appendChild(incButton)
-document.body.appendChild(decButton)
+document.body.appendChild(incButton);
+document.body.appendChild(decButton);
